b9a3edc0043f934003242f41c6ddc1b3e699249a
Ercan-home <ercanerdogan@gmail.com>
2020-03-25 18:12:35 +0300

    numbers has been summed

---
+++ sumOfOddNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace sumOfOddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many numbers do you want to enter : ");
+            int numberCount = Convert.ToInt32(Console.ReadLine());
+            int numberSum = 0;
+            int number = 1;
+            for (int i = 0; i < numberCount; i++)
+            {
+                Console.WriteLine(number);
+                numberSum += number;
+                number += 2;
+
+            }
+
+            Console.WriteLine($"Sum of numbers you were given : {numberSum.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
